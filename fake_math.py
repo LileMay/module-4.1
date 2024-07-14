83035cff50f0f53e201c34ede7efa8d36c66cbfd
LileMay <lilemay31@gmail.com>
2024-07-14 17:51:06 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(a, b):
+    if b == 0:
+        return "ошибка"
+    else:
+        c = a / b
+        return c
+
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+
+
+def divide(a, b):
+    if b == 0:
+        return inf
+    else:
+        c = a / b
+        return c
+
+
